326b14fd38dcbf8cbc7c5b245ee894e57c177ade
EdneyMesquita <edneyphp@outlook.com>
2021-08-07 21:02:34 -0300

    Initial Commit

---
+++ SwiftUI-TabView/SwiftUI_TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TabViewApp.swift
+//  SwiftUI-TabView
+//
+//  Created by Edney Mesquita on 07/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
